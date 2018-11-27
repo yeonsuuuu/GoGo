b43ae5298b5012767214af45d097ce6f91b1340b
Daeun Kim <eileenkim1208@gmail.com>
2018-11-27 10:20:41 +0900

    add react & index.js

---
+++ hello-react/src/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import ReactDOM from "react-dom";
+import { Text } from 'gestalt';
+import 'gestalt/dist/gestalt.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        Drunken Gals
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />, 
+  document.querySelector("#root"));
